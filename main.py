015c61fdfd96cf82689759ebcfe2f79515b6951d
Daniel Centeno <101992463+DCenten0@users.noreply.github.com>
2024-11-05 00:43:31 +0100

    Updated Manager and main
    Moved the WebSocket initialization and methods to the manager for centralized control. Updated data.get['action'] to data['action'] to access dictionaries directly and handle errors more effectively when the action contains invalid data.

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import asyncio
-import websockets
-from server.websocket_server import handle_client
+from  model_manager.model_manager import ModelManager
 
+manager = ModelManager()
 
 async def main():
-    print("WebSocket server is starting...")
-    try:
-
-        #Set the host ip
-        host = "192.168.178.75"
-
-        # Start the WebSocket server
-        start_server = await websockets.serve(handle_client, host , 5000)
-        print(f"Server running on ws://{host}.:5000")
-        
-        await start_server.wait_closed()
-        
-    except Exception as e:
-        print(f"Error starting the server: {e}")
-    finally:
-        print("Server shutdown.")
+    await manager.start_server()
 
 if __name__ == '__main__':
     try:
--- model_manager/model_manager.py
+++ model_manager/model_manager.py
@@ -1,15 +1,24 @@
+import json
+import cv2
+import base64
 import importlib
+import websockets
+import numpy as np
 from pathlib import Path
 
 class ModelManager:
 
+#region Constructor
     def __init__(self):
         self.running_model = None
+#endregion
+
+#region Model Manager Methods
 
     def launch_model(self, data):
 
         try:
-            model_name = str(data.get('model')).lower()
+            model_name = str(data['model']).lower()
 
             if model_name == "base_model": 
                 return {"message": f"Model {model_name} is not supported"}
@@ -32,12 +41,18 @@ class ModelManager:
         except (ModuleNotFoundError, AttributeError) as e:
             return {"error": f"Model {model_name} is not supported or failed to load. Error: {str(e)}"}
 
-    def run_model(self, img):
-        if not self.running_model:
-            return {"error": "No model is currently running"}
+    def run_model(self, data):
         
         try:
-            return self.running_model.run(img)
+            if not self.running_model:
+                return {"error": "No model is currently running"}
+
+            img_str = data['image']
+            img_data = base64.b64decode(img_str)
+            np_arr = np.frombuffer(img_data, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            return self.running_model.run(img)   
         except Exception as e:
             return {"error": f"Error during model execution: {str(e)}"}
 
@@ -85,3 +100,98 @@ class ModelManager:
             models_info[model_name] = model_info[model_name]
 
         return {"models": models_info}
+
+#endregion  
+
+#region WebSocket Server Methods
+
+    #WebSocket Inizialization
+    async def start_server(self):
+        print("WebSocket server is starting...")
+        try:
+
+            #Set the host ip
+            host = "192.168.178.75"
+
+            # Start the WebSocket server
+            self.server = await websockets.serve(self.handle_client, host , 5000)
+            print(f"Server running on ws://{host}:5000")    
+            await self.server.wait_closed()
+            
+        except Exception as e:
+            print(f"Error starting the server: {e}")
+        finally:
+            print("Server shutdown.")
+
+    #Handle actions with the client
+    async def handle_client(self, websocket, path):
+
+        try:
+            async for message in websocket:
+                data = json.loads(message)
+                action = data['action']
+
+                # Print the message received from the client
+                print(f"Message received from client {websocket.remote_address}: " "action: " f"{action}")
+
+                if action == "launch_model":
+                    await self.ws_launch_model(websocket, data)
+                elif action == "get_state":
+                    await self.ws_get_state(websocket)
+                elif action == "get_info":
+                    await self.ws_get_info(websocket)
+                elif action == "stop_model":
+                    await self.ws_stop_model(websocket)
+                elif action == "image_frame":
+                    await self.ws_handle_image_frame(websocket, data)
+                else:
+                    await websocket.send(json.dumps({"error": "Invalid action"}))
+
+        except websockets.ConnectionClosed as e:
+            print(f"Client {websocket.remote_address} disconnected: {e}")
+        except json.JSONDecodeError:
+            await websocket.send(json.dumps({"error": "Invalid JSON format"})) 
+        except Exception as e:
+            await websocket.send(json.dumps({"error": f"Unexpected error: {str(e)}"}))
+
+    #WebSocket to launch the model
+    async def ws_launch_model(self, websocket, data):
+        try:
+            response = self.launch_model(data)
+            await websocket.send(json.dumps(response))  
+        except Exception as e:
+            await websocket.send(json.dumps({"error": str(e)}))
+
+    #WebSocket to get the current model state
+    async def ws_get_state(self, websocket):
+        try:
+            response = self.get_state()
+            await websocket.send(json.dumps(response))
+        except Exception as e:
+            await websocket.send(json.dumps({"error": str(e)}))
+
+    #WebSocket to know about the available models
+    async def ws_get_info(self, websocket):
+        try:
+            response = self.models_info()
+            await websocket.send(json.dumps(response))
+        except Exception as e:
+            await websocket.send(json.dumps({"error": str(e)}))
+
+    #WebSocket to stop the current model
+    async def ws_stop_model(self, websocket):
+        try:
+            response = self.stop_model()
+            await websocket.send(json.dumps(response))
+        except Exception as e:
+            await websocket.send(json.dumps({"error": str(e)}))
+
+    #WebSocket to manage all the frames when a model is running
+    async def ws_handle_image_frame(self, websocket, data):
+        try:
+            response = self.run_model(data)
+            await websocket.send(json.dumps(response))  
+        except Exception as e:
+            await websocket.send(json.dumps({"error": str(e)}))
+
+#endregion
