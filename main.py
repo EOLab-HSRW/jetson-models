d6e8d50d8bd73ed793db97f5cd6d0c13089d189a
Daniel Centeno <101992463+DCenten0@users.noreply.github.com>
2025-05-23 03:13:24 -0600

    Add --debug flag to enable debug mode on server startup
    Add command-line argument to toggle debug mode when starting server; updated server entrypoint to accept a --debug boolean parameter that enables performance reporting during execution

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import asyncio
+import argparse
 from  model_manager.model_manager import ModelManager
+
+parser = argparse.ArgumentParser(
+    description='Jetson Inference AI Model Manager Server')
+
+#Params for debugging
+parser.add_argument("--debug", type=bool, default=False,
+                    help='Specify if the server will generate a preformance report of every single executed action')
+
+args = parser.parse_args()
 
 manager = ModelManager()
 
 async def main():
-    await manager.start_server()
+    await manager.start_server(args)
 
 if __name__ == '__main__':
     try:
