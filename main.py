a1daa5c0579b2699188cbbfbf0fe4f9c98224730
Daniel Centeno <101992463+DCenten0@users.noreply.github.com>
2025-06-03 23:55:47 -0600

    Added --delete-datasets flag to delete or not the dataset flag
    Added a command-line argument to delete the complete dataset folder from the main folder project if exist. The --delete-datasets is a boolean parameter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import shutil
 import asyncio
 import argparse
+from pathlib import Path
 from  model_manager.model_manager import ModelManager
 
 parser = argparse.ArgumentParser(
@@ -8,6 +10,10 @@ parser = argparse.ArgumentParser(
 #Params for debugging
 parser.add_argument("--debug", type=bool, default=False,
                     help='Specify if the server will generate a preformance report of every single executed action')
+
+#Params for dataset
+parser.add_argument("--delete-datasets", type=bool, default=False,
+                    help='Specify if the server will delete all the created datasets after the server is closed')
 
 args = parser.parse_args()
 
@@ -21,4 +27,8 @@ if __name__ == '__main__':
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main())
     except KeyboardInterrupt:
+        dataset_path = Path("datasets")
+        if args.delete_datasets and Path(dataset_path).is_dir():
+            shutil.rmtree(dataset_path)
+            print('\nDataset folder and its content removed')
         print("\nKeyboardInterrupt: Server shutdown.")
