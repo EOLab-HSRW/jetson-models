8b9be0afcc163d6974b7bd2df2476e0118c92bc7
Daniel Centeno <101992463+DCenten0@users.noreply.github.com>
2025-10-25 03:18:39 -0600

    Add methods output type

--- models/base_model.py
+++ models/base_model.py
@@ -7,32 +7,32 @@ class BaseModel(ABC):
 
     @property
     @abstractmethod
-    def model_name(self):
+    def model_name(self) -> str:
         pass
 
     @property
     @abstractmethod
-    def variant(self):
+    def variant(self) -> str:
         pass
 
     @property
     @abstractmethod
-    def is_custom(self):
+    def is_custom(self) -> bool:
         pass
 
     @abstractmethod
-    def launch(self, data: Dict[str, Any]):
+    def launch(self, data: Dict[str, Any]) -> bool:
         raise NotImplementedError()
 
     @abstractmethod
-    def run(self, img: np.ndarray):
+    def run(self, img: np.ndarray) -> any:
         raise NotImplementedError()
     
     @abstractmethod
-    def stop(self):
+    def stop(self) -> None:
         raise NotImplementedError()
     
     @staticmethod
     @abstractmethod
-    def get_opts():
+    def get_opts() -> dict:
         raise NotImplementedError()
